4d8ecd233935740bd0f009431ed0ed80613edd48
Tristanw3 <tristanw3@outlook.com>
2022-09-02 23:56:33 +1000

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ app.use(express.static('./frontend'))
 
 const port = 8000;
 let server = app.listen(port, function () {
-   console.log("Example app listening at http://localhost:%s", port)
+   console.log("Listening at http://localhost:%s", port)
 })
