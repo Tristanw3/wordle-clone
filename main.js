08793c4a2191621c74e7e6a68ad595902dfab264
unknown <tristanw3@outlook.com>
2022-09-15 18:19:18 +1000

    edits

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let app = express();
 app.use(express.static('./frontend'))
 
 const port = 8000;
-let server = app.listen(port, function () {
+
+app.listen(port, function () {
    console.log("Listening at http://localhost:%s", port)
 })
